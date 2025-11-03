2cdecbe3de2fa123d8d7b560e2225490b914017e
RoxasYTB <72387595+RoxasYTB@users.noreply.github.com>
2025-11-03 11:01:29 +0100

    Push forcé

--- lua/entities/prop_door_trigger.lua
+++ lua/entities/prop_door_trigger.lua
@@ -1,26 +1,26 @@
 AddCSLuaFile();
-ENT.Type = "anim";
-ENT.Base = "base_anim";
+ENT.Type = "brush";
+ENT.Base = "base_trigger";
 function ENT:Initialize()
 	self:SetModel("models/hunter/plates/plate1x2.mdl");
 	self:SetColor(Color(255, 255, 255, 0));
 	self:SetRenderMode(RENDERMODE_TRANSALPHA);
 	self:SetMaterial("models/debug/debugwhite");
-	self:SetSolid(SOLID_BBOX);
-	self:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE);
 	self:DrawShadow(false);
 	self:SetNoDraw(false);
-	if self.DoorTriggerType == nil then
+	self:SetTrigger(true);
+	self:UseTriggerBounds(true, 16);
+	if not self.DoorTriggerType then
 		self:Remove();
-		return;
 	end;
 end;
 function ENT:StartTouch(ent)
-	if ent:IsPlayer() then
-		if self.DoorTriggerType == "open" then
-			RunConsoleCommand("open_first_door");
-		elseif self.DoorTriggerType == "close" then
-			RunConsoleCommand("close_first_door");
-		end;
+	if not ent:IsPlayer() then
+		return;
+	end;
+	if self.DoorTriggerType == "open" then
+		RunConsoleCommand("open_first_door");
+	elseif self.DoorTriggerType == "close" then
+		RunConsoleCommand("close_first_door");
 	end;
 end;
